45f484b96a1f1d16484c069b311f35455e76b26d
zomervinicius <vinicius.zomer123@gmail.com>
2019-05-25 19:20:52 -0300

    change date to BR format

--- src/Components/TimeLineList/TimeLineList.js
+++ src/Components/TimeLineList/TimeLineList.js
@@ -1,12 +1,12 @@
-import React from "react";
-import TimeLineListItem from "../TimeLineListItem/TimeLineListItem";
-import { connect } from "react-redux";
-import { Spinner, Heading } from 'evergreen-ui'
-import { VerticalTimeline } from "react-vertical-timeline-component";
-import { acumulateByYear } from '../../Helpers/acumulator'
-const TimeLineList = ({ repositories , repositoriesByYear , ...props }) => {
+import { Heading, Spinner } from "evergreen-ui"
+import React from "react"
+import { connect } from "react-redux"
+import { VerticalTimeline } from "react-vertical-timeline-component"
+import { acumulateByYear } from "../../Helpers/acumulator"
+import TimeLineListItem from "../TimeLineListItem/TimeLineListItem"
+const TimeLineList = ({ repositories, repositoriesByYear, ...props }) => {
   if (repositories.loading) {
-    return <Spinner margin='auto' marginTop={10} />
+    return <Spinner margin="auto" marginTop={10} />
   }
 
   if (repositories.loaded) {
@@ -14,23 +14,40 @@ const TimeLineList = ({ repositories , repositoriesByYear , ...props }) => {
       <VerticalTimeline>
         {repositories.repositories.map(repo => {
           console.log(repo.git_commits_url)
-          return <TimeLineListItem description={repo.description} language={repo.language} repoName={repo.name} key={repo.id} date={repo.created_at} />;
+          return (
+            <TimeLineListItem
+              description={repo.description}
+              language={repo.language}
+              repoName={repo.name}
+              key={repo.id}
+              date={repo.created_at}
+            />
+          )
         })}
       </VerticalTimeline>
-    );
+    )
   }
 
-  return <Heading size={900} marginTop="default">GitHub Timeline</Heading>;
-};
+  return (
+    <Heading size={900} marginTop="default">
+      GitHub Timeline
+    </Heading>
+  )
+}
 
 function mapStateToProps({ repositories }) {
   return {
-    repositoriesByYear: acumulateByYear(repositories.repositories, 'created_at'),
+    repositoriesByYear: acumulateByYear(
+      repositories.repositories,
+      "created_at"
+    ),
     repositories: {
       ...repositories,
-      repositories: repositories.repositories.sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at)).filter((repo) => !repo.private && !repo.archived && !repo.forked)
+      repositories: repositories.repositories
+        .sort((a, b) => Date.parse(b.created_at) - Date.parse(a.created_at))
+        .filter(repo => !repo.private && !repo.archived && !repo.forked)
     }
-  };
+  }
 }
 
-export default connect(mapStateToProps)(TimeLineList);
+export default connect(mapStateToProps)(TimeLineList)
