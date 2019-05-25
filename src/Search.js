45680df60d3b7fb3d8e963a58c833fa48f07af2b
Fernando Souza <fernandostorm1@gmail.com>
2019-05-24 21:19:23 -0300

    list styles

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, Fragment } from 'react'
 import { SearchInput, Autocomplete } from "evergreen-ui"
 import { connect } from 'react-redux'
 import { searchUsers } from './redux/reducers/users';
-
+ 
 const Search = (props) => {
     const [searchString, setSearchString] =  useState('');
     const { dispatch } = props;
@@ -20,7 +20,16 @@ return (<Fragment>
             width={500}
             autoFocus
         />
-        <ul>
+        <ul style={{
+            display: 'inline-block', 
+            position:'absolute', 
+            top:'50px', 
+            listStyle: 'none',
+            left: '50%',
+            marginRight: '-200px',
+            width: '400px', 
+            zIndex:'100'
+        }}>
             {props.users.suggestions.map(user => <li key={user.id}>{user.login}</li>)}
         </ul>
     </Fragment>
