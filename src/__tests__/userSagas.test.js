de609af3e47e18fd050930db195c3f5686b69f33
Fernando Souza <fernandostorm1@gmail.com>
2019-05-27 00:14:09 -0300

    Fix typo problems

--- src/__tests__/userSagas.test.js
+++ src/__tests__/userSagas.test.js
@@ -1,5 +1,5 @@
-import sagaHelper from 'redux-saga-testing'
-import { call, put } from 'redux-saga/effects'
+import sagaHelper from 'redux-saga-testing';
+import { call, put } from 'redux-saga/effects';
 
 import { 
     getUsers, 
@@ -7,30 +7,31 @@ import {
     setUsers
 } from "../redux/sagas/users";
 
-import { initialState, axiosMock } from '../Helpers/testUtils'
+import { initialState, axiosMock } from '../Helpers/testUtils';
 
 const params = {
     searchString: 'someone',
-}
+};
 
 it('should call axios.request with appropriate parameters for users search', () => {
-    getUsersHandle(params.searchString, axiosMock)
+    getUsersHandle(params.searchString, axiosMock);
     expect(axiosMock.request).toHaveBeenCalledTimes(1);
     
     expect(axiosMock.request.mock.calls[0][0]).toMatchObject({
         method: "get",
         url: `http://api.github.com/search/users?q=${params.searchString}`
-      })    
+      });
 });
 
 
-const itUser = sagaHelper(getUsers(axiosMock, params))
+const itUser = sagaHelper(getUsers(axiosMock, params));
   
 itUser('should call getUsersHandle', result => {
-    expect(result).toEqual(call(getUsersHandle, params.searchString, axiosMock))
-    return {items: initialState.users.suggestions}
+    expect(result).toEqual(call(getUsersHandle, params.searchString, axiosMock));
+    return {items: initialState.users.suggestions};
 })
 
 itUser('should put setUsers', result => {
-    expect(result).toEqual(put(setUsers(initialState.users.suggestions.slice(0,5))))
+    expect(result).toEqual(put(setUsers(initialState.users.suggestions.slice(0,5))));
 })
+
