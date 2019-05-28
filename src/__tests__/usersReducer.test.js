ffdcefc73c5164e823e79eb95574a248888f90b0
Fernando Souza <fernandostorm1@gmail.com>
2019-05-27 22:50:54 -0300

    Include more tests for sagas and reducers

--- src/__tests__/usersReducer.test.js
+++ src/__tests__/usersReducer.test.js
@@ -1,16 +1,43 @@
 import React from 'react'
-import users, { Types } from '../redux/reducers/users'
+import users, { Types, searchUsers, setUser } from '../redux/reducers/users'
 import { initialState } from '../Helpers/testUtils'
 
-describe('Users reducer', () => {
+describe('Test users reducer', () => {
     const initiaStorelState = {
         suggestions: [],
         user: {},
     }
 
+    it('Should export the correct types', () => {
+        expect(Types).toEqual(
+            {
+                GET_USERS: 'users/GET_USERS',
+                SET_USERS: 'users/SET_USERS',
+                SET_USER: 'users/SET_USER',
+                RESET_USER: 'users/RESET_USER'
+            }            
+        )
+    });
+
     it('should return the initial state', () => {
         expect(users(undefined, {})).toEqual(initiaStorelState);
     });
+
+    it('should dispatch GET_USERS', () => {
+        expect(searchUsers('somebody')).toEqual({
+            type: Types.GET_USERS,
+            searchString: 'somebody',
+        });
+    });
+
+
+    it('should dispatch SET_USER', () => {
+        expect(setUser(initialState.user)).toEqual({
+            type: Types.SET_USER,
+            user: initialState.user,
+        });
+    });
+
 
     it('should handle GET_USERS', () => {
         expect(users(initiaStorelState, {
