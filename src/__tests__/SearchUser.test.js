bac4de6a1061b6b3e5e7ff5511d16c8af8e3738b
Fernando Souza <fernandostorm1@gmail.com>
2019-05-28 22:59:40 -0300

    Include SearchUser tests

--- src/__tests__/SearchUser.test.js
+++ src/__tests__/SearchUser.test.js
@@ -1,27 +1,40 @@
 import React from 'react';
-import { within, waitForElement, cleanup, fireEvent } from 'react-testing-library';
+import { cleanup, fireEvent } from 'react-testing-library';
 
-import { renderWithRedux } from '../Helpers/testUtils';
+import { renderWithRedux, initialState } from '../Helpers/testUtils';
 import SearchUser from '../Components/Search/SearchUser';
 
 afterEach(cleanup);
 
 describe('Test SearchUser component', () => {
-//    const setSearchString = jest.fn();
-//    const useStateSpy = jest.spyOn(React, 'useState')
-//    useStateSpy.mockImplementation((searchString) => [searchString, setSearchString]);
 
-    it('Rende SearchUser correctly', () => {
-        const { getByTestId, container } = renderWithRedux(<SearchUser />);
+    it('Should render SearchUser correctly', () => {
+        const { getByTestId } = renderWithRedux(<SearchUser />);
     
         const searchInput = getByTestId('search-input');
 
         fireEvent.change(searchInput, { target: { value: 'somebody' } });
         expect(searchInput.value).toBe('somebody');
-        fireEvent.keyPress(searchInput, { key: "Enter", code: 13, charCode: 13 });
 
-        expect(searchInput.value).toBe('');
-        //expect(setSearchString).toHaveBeenCalledWith('somebody');
-        //expect(container.state('searchString')).toEqual('somebody');
+        fireEvent.keyDown(searchInput, { key: "Enter", code: 13, keyCode: 13, charCode: 13 });
+        
+        expect(searchInput.value).toBe('somebody');
+
     });
+
+    it('Should render users suggestions', () => {
+        const { getByTestId, container } = renderWithRedux(<SearchUser />, {initialState: initialState});
+    
+        const usersList = container.querySelectorAll('#userDiv')
+
+        expect(usersList.length).toBe(7);
+
+        const searchInput = getByTestId('search-input');
+
+        fireEvent.keyDown(searchInput, { key: "Arrow", code: 40, keyCode: 40, charCode: 40 });
+        fireEvent.keyDown(document.activeElement, { key: "Arrow", code: 40, keyCode: 40, charCode: 40 });        
+        fireEvent.keyDown(document.activeElement, { key: "Arrow", code: 40, keyCode: 40, charCode: 40 });
+
+        expect(document.activeElement).toEqual(usersList[2])
+    });    
 });
